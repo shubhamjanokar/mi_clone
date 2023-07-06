98ba8a6ca03868c028ed73ed48e60f5ab5aec449
shubham janokar <shubhamgjanokar@gmail.com>
2023-07-06 11:03:18 +0530

    day5

--- mistoreclone/src/App.js
+++ mistoreclone/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider'
 import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
           <Router>
              <PreNavbar/>
              <Navbar/>
+             <Slider/>
           </Router>  
           );
 }
---
+++ mistoreclone/src/components/Slider.js
@@ -0,0 +1,18 @@
+import { Carousel } from 'bootstrap'
+import React from 'react'
+
+const Slider = () => {
+  return (
+  <>
+  
+  </>
+    // <Carousel>
+    //     {/* <Carousel.Item>
+
+    //     </Carousel.Item> */}
+
+    // </Carousel>
+  )
+}
+
+export default Slider
